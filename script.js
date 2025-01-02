46629fcce106868fb1bf493819a45d93c4be8705
ALX <130580740+herminard@users.noreply.github.com>
2025-01-01 21:04:28 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,1 @@
-// Assurez-vous de charger l'image avant d'essayer de la dessiner
-// const truckImage = new Image();
-// truckImage.src = 'images/2956028-camion-vehicule-vue-de-dessus-vecteur-conception-gratuit-vectoriel.jpg';
 
-// Dessinez l'image du camion lorsque l'image est chargée
-// truckImage.onload = function() {
-//     const canvas = document.getElementById('gameCanvas');
-//     const ctx = canvas.getContext('2d');
-
-//     // Redimensionner l'image à 80% de sa taille d'origine
-//     const scale = 0.8;
-//     const scaledWidth = truckImage.width * scale;
-//     const scaledHeight = truckImage.height * scale;
-
-//     // Exemple de dessin de l'image du camion au centre du canevas
-//     const truckX = canvas.width / 2 - scaledWidth / 2;
-//     const truckY = canvas.height / 2 - scaledHeight / 2;
-//     ctx.drawImage(truckImage, truckX, truckY, scaledWidth, scaledHeight);
-// };
